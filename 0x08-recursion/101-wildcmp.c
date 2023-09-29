31cb709cc42b2e341bc7b4c1b850eafd77aa82be
Hussain <hussain4me2008@gmail.com>
2023-09-29 09:10:21 -0700

    Update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,9 +19,9 @@ return (wildcmp(s1, s2 + 1));
 return (*s2 == '\0');
 }
 
-if (*s2 == "*")
+if (*s2 == '*')
 {
-return (wildcmp(s1 + 1) || wildcmp(s1, s2 + 1));
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 }
 else if (*s1 == *s2)
 {
